01042313f747d748d77c811c05883c3b202cff8c
ArisAgnew <aris.sektor@gmail.com>
2019-03-31 13:00:50 +0300

    Create IntStruct.cs

---
+++ AgnewMonad/Containers/IntStruct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AgnewMonad
+{
+    public struct IntStruct
+    {
+        private static readonly object sync = new object();
+
+        public int Value { get; }
+
+        public IntStruct(in int value) => Value = value;
+
+        public IntStruct Interlock(Func<int, IntStruct> func)
+        {
+            if (Value == default)
+            {
+                lock (sync)
+                {
+                    return new IntStruct(default);
+                }
+            }
+            return func(Value);
+        }
+
+        public static implicit operator IntStruct(int value) => new IntStruct(value);
+        public static explicit operator int(IntStruct intContainer) => intContainer.Value;
+    }
+}
