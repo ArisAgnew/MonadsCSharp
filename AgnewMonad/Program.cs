7a8b51392cb1cec921e04fffcbc05df107e0895e
ArisAgnew <aris.sektor@gmail.com>
2019-03-31 13:00:54 +0300

    Update Program.cs

--- AgnewMonad/Program.cs
+++ AgnewMonad/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
+
+using static System.Console;
 
 namespace AgnewMonad
 {
@@ -7,19 +8,38 @@ namespace AgnewMonad
     {
         static void Main(string[] args)
         {
-            var _int = new IntContainer<int>(10)
-                .Interlock(v => new IntContainer<int>(v + 10))
-                .Interlock(v => new IntContainer<int>(v * 4))
-                .Interlock(v => new IntContainer<int>(v + (v * 1/4)));
+            /* The solution by using a struct is actually more fine-drawn & shorter as compared with using a class. 
+             * There is no need to create an instance using a struct.
+             */
+            {
+                IntStruct _intStructInitial = 10;
 
-            var _string = new StringContainer<string>(new StringBuilder("They will all"))
-                .Interlock(v => new StringContainer<string>(v.Append(" ").Append("cease")))
-                .Interlock(v => new StringContainer<string>(v.Append(" ").Append("to exist")));
+                var _intStruct = _intStructInitial
+                    .Interlock(v => v + 10)
+                    .Interlock(v => v * 4)
+                    .Interlock(v => v + (v * 1 / 4));
 
-            Console.WriteLine(_int.Value);
-            Console.WriteLine(_string.StringBuilder.ToString());
+                WriteLine(_intStruct.Value);
+            }
 
-            Console.Read();
+            {
+                var _int = new IntContainer<int>(10)
+                  .Interlock(v => new IntContainer<int>(v + 10))
+                  .Interlock(v => new IntContainer<int>(v * 4))
+                  .Interlock(v => new IntContainer<int>(v + (v * 1 / 4)));
+
+                WriteLine(_int.Value);
+            }
+
+            {
+                var _string = new StringContainer<string>(new StringBuilder("They will all"))
+                    .Interlock(v => new StringContainer<string>(v.Append(" ").Append("cease")))
+                    .Interlock(v => new StringContainer<string>(v.Append(" ").Append("to exist")));
+
+                WriteLine(_string.StringBuilder.ToString());
+            }
+            
+            Read();
         }
     }
 }
